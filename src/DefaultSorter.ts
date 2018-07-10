61fc14da07a695f13e586c1a785210cdc43e7bd2
Gowtham Gopalakrishnan <gowthamgts@users.noreply.github.com>
2018-07-10 11:02:47 +0530

    Adding support for fontawesome

--- src/DefaultSorter.ts
+++ src/DefaultSorter.ts
@@ -6,8 +6,8 @@ import {DataTable, SortEvent} from "./DataTable";
     template: `
         <a style="cursor: pointer" (click)="sort()" class="text-nowrap">
             <ng-content></ng-content>
-            <span *ngIf="isSortedByMeAsc" class="glyphicon glyphicon-triangle-top" aria-hidden="true"></span>
-            <span *ngIf="isSortedByMeDesc" class="glyphicon glyphicon-triangle-bottom" aria-hidden="true"></span>
+            <span *ngIf="isSortedByMeAsc" class="glyphicon glyphicon-triangle-top fa fa-sort-up" aria-hidden="true"></span>
+            <span *ngIf="isSortedByMeDesc" class="glyphicon glyphicon-triangle-bottom fa fa-sort-down" aria-hidden="true"></span>
         </a>`
 })
 export class DefaultSorter implements OnInit {
